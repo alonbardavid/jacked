8311a2065bf750643bab82da664b5423b0e3c01e
alon bar david <me@alonbd.com>
2018-04-17 16:03:17 +0300

    added private=false to all package.json files

--- packages/typescript-validator/src/index.ts
+++
@@ -1,9 +0,0 @@
-import {configureBuilder} from '@jacked/core';
-import {validator} from './validator';
-
-
-const formBuilder = configureBuilder({
-    validator:validator,
-});
-
-export {formBuilder};
